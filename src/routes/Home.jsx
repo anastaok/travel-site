b145fd8718bbb73b4e6b094ab80b3efa67ef3ffd
Anastasiia <anasta_ok@mail.ru>
2022-08-07 21:15:35 +0300

    added video background

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import Video from "../components/Video";
 
 const Home = () => {
   return (
     <div>
       <NavBar />
+      <Video />
     </div>
   );
 };
