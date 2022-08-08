48027cf06a9a30bf15174ca638afd4cc78746c8d
Anastasiia <anasta_ok@mail.ru>
2022-08-08 15:25:16 +0300

    added hero-img component and change navbar style

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as NavBar } from "./NavBar/NavBar";
 export { default as Video } from "./Video/Video";
 export { default as Footer } from "./Footer/Footer";
+export { default as HeroImg } from "./HeroImg/HeroImg";
---
+++ src/routes/Pricing.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Footer, HeroImg, NavBar } from "../components";
+
+const Pricing = () => {
+  return (
+    <div>
+      <NavBar />
+      <HeroImg heading="PRICING" text="Choose your trip!" />
+      <Footer />
+    </div>
+  );
+};
+
+export default Pricing;
